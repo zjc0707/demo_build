f827af38bb8b58728bc2d58d0d8ca40cf9af9f76
zjc <624779572@qq.com>
2019-11-19 15:21:19 +0800

    添加json .net for unity解决json在webgl无法使用的问题

--- Assets/Plugins/Json/Json.cs
+++ Assets/Plugins/Json/Json.cs
@@ -38,6 +38,7 @@ public class Json : MonoBehaviour
                 ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
                 NullValueHandling = NullValueHandling.Ignore
             });
+
     }
 
 
