5948c82e90e8a69b337b769d0c9334150a2357f9
zjc <624779572@qq.com>
2020-02-17 19:14:06 +0800

    将地面的网格改为使用重复贴图

--- Assets/Scripts/Floor/Floor.cs
+++ Assets/Scripts/Floor/Floor.cs
@@ -6,15 +6,18 @@ public class Floor : BaseUniqueObject<Floor>
 {
     public int x = 10;
     public int z = 11;
+    private Material material;
     private void Awake()
     {
+        material = this.transform.GetComponent<Renderer>().sharedMaterials[0];
         Load();
     }
     public void Load()
     {
         this.transform.localScale = new Vector3(x, z, 1);
         this.transform.localPosition = new Vector3(x / 2f, 0, z / 2f);
-        FloorTile.current.Load(x, z);
+        // FloorTile.current.Load(x, z);
+        material.SetTextureScale("_MainTex", new Vector2(x, z));
     }
     public void Load(int x, int z)
     {
--- Assets/Scripts/Floor/FloorTile.cs
+++ Assets/Scripts/Floor/FloorTile.cs
@@ -4,35 +4,35 @@ using UnityEngine;
 
 public class FloorTile : BaseUniqueObject<FloorTile>
 {
-    private List<GameObject> objList = new List<GameObject>();
-    public const float thickness = 0.2f;
-    public void Load(int x, int z)
-    {
-        Clear();
+    // private List<GameObject> objList = new List<GameObject>();
+    public const float thickness = 0.0f;
+    // public void Load(int x, int z)
+    // {
+    //     Clear();
 
-        Vector3 start = new Vector3(0.5f, 0, 0.5f);
-        for (int i = 0; i < x; i++)
-        {
-            for (int j = 0; j < z; j++)
-            {
-                Transform target = GameObject.CreatePrimitive(PrimitiveType.Cube).transform;
-                Vector3 vector3 = start + Vector3.right * i + Vector3.forward * j;
-                target.name = string.Format("({0},{1})", i, j);
-                target.parent = this.transform;
-                target.localPosition = vector3;
-                target.localScale = new Vector3(1, thickness, 1);
-                target.GetComponent<Renderer>().sharedMaterial = ResourceStatic.GREY;
-                target.gameObject.SetActive(true);
-                objList.Add(target.gameObject);
-            }
-        }
-    }
-    private void Clear()
-    {
-        foreach (GameObject obj in objList)
-        {
-            DestroyImmediate(obj);
-        }
-        objList.Clear();
-    }
+    //     Vector3 start = new Vector3(0.5f, 0, 0.5f);
+    //     for (int i = 0; i < x; i++)
+    //     {
+    //         for (int j = 0; j < z; j++)
+    //         {
+    //             Transform target = GameObject.CreatePrimitive(PrimitiveType.Cube).transform;
+    //             Vector3 vector3 = start + Vector3.right * i + Vector3.forward * j;
+    //             target.name = string.Format("({0},{1})", i, j);
+    //             target.parent = this.transform;
+    //             target.localPosition = vector3;
+    //             target.localScale = new Vector3(1, thickness, 1);
+    //             target.GetComponent<Renderer>().sharedMaterial = ResourceStatic.GREY;
+    //             target.gameObject.SetActive(true);
+    //             objList.Add(target.gameObject);
+    //         }
+    //     }
+    // }
+    // private void Clear()
+    // {
+    //     foreach (GameObject obj in objList)
+    //     {
+    //         DestroyImmediate(obj);
+    //     }
+    //     objList.Clear();
+    // }
 }
